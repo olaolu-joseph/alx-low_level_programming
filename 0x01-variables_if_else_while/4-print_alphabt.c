fa3ad7fa447826308183915649630155ab4d3157
Joseph <olaolu.ajibola@gmail.com>
2022-10-14 17:10:55 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-	char low, e, q;
+char low, e, q;
 
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
-	
-	return (0);
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
