f101929901270ae638cadd5c690afb8b2b5fd7e9
Joseph <olaolu.ajibola@gmail.com>
2022-10-14 17:21:37 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,11 @@ int main(void)
 	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
-	{for (p = d + 1; p <= '9'; p++)
+	{
+		for (p = d + 1; p <= '9'; p++)
 		{
 			for (q = p + 1; q <= '9'; q++)
+			{
 				if ((p != d) != q)
 				{
 					putchar(d);
@@ -26,9 +28,9 @@ int main(void)
 					putchar(' ');
 				}
 			}
+		}
 	}
-}
 	putchar('\n');
 
 	return (0);
-	}
+}
