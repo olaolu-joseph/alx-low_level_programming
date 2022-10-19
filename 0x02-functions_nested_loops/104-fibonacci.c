ca1990bab27ea8a42e844ebcf7d77a55f2c208d2
Joseph <olaolu.ajibola@gmail.com>
2022-10-19 07:43:52 -0700

    my Functions, nested loops commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,8 +4,9 @@
  * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int count;
